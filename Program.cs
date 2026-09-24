9c373094cd941a15927ab25a96ca95efa491e467
Josué Bayidikila <josue-dev@outlook.fr>
2026-09-24 20:18:38 +0200

    Practice variable declaration and scope

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("C# foundations ready");
+﻿string screeningCode = "EV-204";
+int availableSeats = 72;
+bool doorsOpen = true;
+char roomLetter = 'F';
+
+Console.WriteLine(screeningCode);
+Console.WriteLine(availableSeats);
+Console.WriteLine(doorsOpen);
+Console.WriteLine(roomLetter);
+availableSeats = 68;
+Console.WriteLine(availableSeats);
+int checkedTickets;
+checkedTickets = 14;
+Console.WriteLine(checkedTickets);
+{
+	string employeeCode = "EMP-31";
+	Console.WriteLine(employeeCode);
+}
